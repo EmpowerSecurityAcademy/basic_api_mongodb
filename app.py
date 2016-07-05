f2e275b5fab55bf016d6d5b6a351935616364a9a
sheltowt <sheltowt@gmail.com>
2016-07-05 09:42:30 -0400

    adding updates

--- app.py
+++ app.py
@@ -2,59 +2,74 @@ import json
 import pymongo
 from import_config import load_config
 from pymongo import MongoClient
-from flask import Flask, jsonify, request, make_response
+from flask import Flask, jsonify, request, make_response, g
 from bson.json_util import dumps
 from bson.objectid import ObjectId
 
-app = Flask(__name__)
+basic_api = Flask(__name__)
 
 config = load_config()
 client = MongoClient(config["database"]["connection_url"])
 
 db = client[config['database']['database_name']]
-collection = db[config['database']['collection_name']]
+basic_api.conn = db[config['database']['collection_name']]
 
 url_root = '/todo/api/v2.0/'
 
+def format_json(element):
+	converted = dumps(element)
+	modified = json.loads(converted)
+	new_task = {}
+	new_task["id"] = str(modified["_id"]['$oid'])
+	new_task["description"] = modified["description"]
+	new_task["title"] = modified["title"]
+	new_task["done"] = modified["done"]
+	print(new_task)
+	return new_task
 
-@app.route(url_root+'tasks', methods=['GET', 'POST', 'PUT'])
+@basic_api.route(url_root+'tasks', methods=['GET', 'POST', 'PUT'])
 def do_tasks():
 	if request.method == 'GET':
-		data = collection.find()
-		return make_response(dumps(data), 200)
+		data = basic_api.conn.find()
+		response = []
+		for element in data:
+			print(element)
+			formatted_element = format_json(element)
+			response.append(formatted_element)
+		return make_response(dumps(response), 200)
 
 	if request.method == 'POST':
 		content = request.get_json(silent=True)
-		result = collection.insert_one(content)
+		result = basic_api.conn.insert_one(content)
 		return jsonify({'id': str(result.inserted_id)})
 
 	return make_response(jsonify({'status_code': 500}), 500)
 
 # RESTFUL operations related to a specific task
 
-@app.route(url_root+'tasks/<task_id>', methods=['GET', 'PUT', 'DELETE'])
+@basic_api.route(url_root+'tasks/<task_id>', methods=['GET', 'PUT', 'DELETE'])
 def do_task(task_id):
 	if request.method == 'GET':
-		data = collection.find_one({"_id": ObjectId(task_id)})
+		data = basic_api.conn.find_one({"_id": ObjectId(task_id)})
 		return make_response(dumps(data), 200)
 
 	if request.method == 'PUT':
 		content = request.get_json(silent=True)
-		result = collection.update_one(
+		result = basic_api.conn.update_one(
 			{"_id": task_id},
 			{"$set": {"title": content["title"], 
 						"description": content["description"], 
 						"done": content["done"]}}
 		)
-		data = collection.find_one({"_id": ObjectId(task_id)})
+		data = basic_api.conn.find_one({"_id": ObjectId(task_id)})
 		return make_response(dumps(data), 200)
 
 	if request.method == 'DELETE':
-		result = collection.delete_one({"_id": task_id})
+		result = basic_api.conn.delete_one({"_id": task_id})
 		return make_response(jsonify({'status_code': 200}), 200)
 
 	return make_response(jsonify({'status_code': 500}), 500)
 
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5002)
+    basic_api.run(debug=True, host='0.0.0.0', port=5002)
---
+++ test_app.py
@@ -0,0 +1,80 @@
+import unittest
+import app
+from import_config import load_config
+import json 
+from pymongo import MongoClient
+
+config = load_config()
+
+client = MongoClient(config['test_database']["connection_url"])
+db = client[config['test_database']['database_name']]
+app.basic_api.conn = db[config['test_database']['collection_name']]
+
+tasks = [
+    {
+        'id': 1,
+        'title': 'Buy groceries',
+        'description': 'Milk, Cheese, Pizza, Fruit, Tylenol',
+        'done': False
+    },
+    {
+        'id': 2,
+        'title': 'Learn Python',
+        'description': 'Need to find a good Python tutorial on the web',
+        'done': False
+    }
+]
+
+class AppTestCases(unittest.TestCase):
+
+    def setUp(self):
+        app.basic_api.conn.remove()
+        app.basic_api.conn.insert_one(tasks[0])
+        app.basic_api.conn.insert_one(tasks[1])
+        self.test_app = app.basic_api.test_client()
+
+    def test_get_tasks(self):
+        response = self.test_app.get('/todo/api/v2.0/tasks')
+        print(response)
+        data = json.loads(response.data)
+        self.assertEqual(data['tasks'], tasks)
+
+    def test_post_tasks(self):
+        response = self.test_app.post('/todo/api/v2.0/tasks', 
+                       data=json.dumps(dict(title='Eat a sandwich', description='Enjoy it a lot', done=0)),
+                       content_type = 'application/json')
+        data = json.loads(response.data)
+        self.assertEqual(data['id'], 3)
+
+    def test_get_task(self):
+        response = self.test_app.get('/todo/api/v2.0/tasks/1')
+        data = json.loads(response.data)
+        self.assertEqual(data['task'], {
+                                            'id': 1,
+                                            'title': 'Buy groceries',
+                                            'description': 'Milk, Cheese, Pizza, Fruit, Tylenol',
+                                            'done': False
+                                        })
+
+    def test_put_task(self):
+        response = self.test_app.put('/todo/api/v2.0/tasks/1', 
+                       data=json.dumps(dict(title='Order from Instacart', description='Milk, Cheese, Pizza, Fruit, Tylenol, Burrito', done=0)),
+                       content_type = 'application/json')
+        data = json.loads(response.data)
+        self.assertEqual(data['task'], {
+                                            'id': 1,
+                                            'title': 'Order from Instacart',
+                                            'description': 'Milk, Cheese, Pizza, Fruit, Tylenol, Burrito',
+                                            'done': False
+                                        })
+
+    def test_delete_task(self):
+        self.test_app.post('/todo/api/v2.0/tasks', 
+                       data=json.dumps(dict(title='Eat a sandwich', description='Enjoy it a lot', done=0)),
+                       content_type = 'application/json')
+        response = self.test_app.delete('/todo/api/v3.0/tasks/3')
+        data = json.loads(response.data)
+        self.assertEqual(int(data['deleted_id']), 3)
+
+if __name__ == '__main__':
+    unittest.main()
